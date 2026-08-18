88101ead9f7a5d34c4cda5b195abf6cb60ecee83
AdityaGhamat <adityaghamat01@gmail.com>
2026-08-18 12:17:42 +0530

    feat:added crm routes

---
+++ app/api/v1/crm/get-customer-history/route.ts
@@ -0,0 +1,37 @@
+import { dbConnect } from "@/lib/database/db";
+import { Customer } from "@/lib/schema/customer";
+import { Job } from "@/lib/schema/job";
+import { NextRequest, NextResponse } from "next/server";
+
+export default async function GET(request: NextRequest) {
+  try {
+    await dbConnect();
+    const q = request.nextUrl.searchParams;
+    const customerId = q.get("cid");
+    const [customer, history] = await Promise.all([
+      Customer.findById(customerId).lean(),
+      Job.find({ customerId: customerId })
+        .select("title status scheduledStart totalAmount")
+        .sort({ scheduledStart: -1 })
+        .lean(),
+    ]);
+    return NextResponse.json({
+      success: true,
+      data: {
+        customer,
+        history,
+      },
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: {
+          code: "SERVER_ERROR",
+          message: "Failed to get customer history",
+        },
+      },
+      { status: 500 },
+    );
+  }
+}
---
+++ app/api/v1/crm/search-customer/route.ts
@@ -0,0 +1,34 @@
+import { dbConnect } from "@/lib/database/db";
+import { Customer, ICustomer } from "@/lib/schema/customer";
+import { NextRequest, NextResponse } from "next/server";
+
+export default async function GET(request: NextRequest) {
+  try {
+    await dbConnect();
+    const q = request.nextUrl.searchParams;
+    const searchTerm = q.get("q");
+    const customers = await Customer.find({
+      $or: [
+        { name: { $regex: searchTerm as string, $options: "i" } },
+        { phone: { $regex: searchTerm as string, $options: "i" } },
+      ],
+    } as any)
+      .limit(10)
+      .lean();
+    return NextResponse.json({
+      success: true,
+      data: customers,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: {
+          code: "SERVER_ERROR",
+          message: "Failed to get customer",
+        },
+      },
+      { status: 500 },
+    );
+  }
+}
